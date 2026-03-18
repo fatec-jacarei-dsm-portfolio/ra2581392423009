287df4df70190044bc1180014bd8fd07e2edfcc6
IgorSantosL <igors.lima12@gmail.com>
2026-03-18 15:03:37 -0300

    Ajusta build do portfolio para publicacao em docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ra2581392423009/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
