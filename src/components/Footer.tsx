e1a783858946e3baa37939ec673074c54d5f9817
GustavoHammes <daffa1632@gmail.com>
2026-06-10 20:02:05 -0300

    updating final project

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="footer">
-      <div className="container footer-content">
-        <p>© 2025 - Igor Lima - FATEC Jacareí - DSM</p>
-      </div>
-    </footer>
-  )
-}
